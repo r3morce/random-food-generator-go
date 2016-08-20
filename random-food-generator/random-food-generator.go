d2a9bd262b9d0d4aee50c99477fc8ee2360b044e
Mathias <roteaugen@gmx.net>
2016-08-20 22:57:51 +0200

    udpate db

--- random-food-generator/random-food-generator.go
+++ random-food-generator/random-food-generator.go
@@ -7,59 +7,70 @@ import (
 )
 
 func main() {
-	fmt.Println("")
 
 	var minRanking int
 	var maxRanking int
 
 	// get all foods with rankings from db
-	var foods = [3]Food{{"pizza", 5}, {"burger", 8}, {"salat", 16}}
+	var foods = [3]Food{{"pizza", 10}, {"burger", 20}, {"salat", 30}}
 
 	// calculate chances
 	var rankings []Ranking
 
 	for i := 0; i <= len(foods)-1; i++ {
-		fmt.Printf("%v\n", foods[i])
 		food := foods[i]
 		maxRanking += food.rank
 		rankings = append(rankings, Ranking{food.name, minRanking, maxRanking})
 		minRanking += food.rank
 	}
 
+	// show chances
+	var ranking Ranking
+	var chance float32
+	fmt.Println("Chances are: ")
+	for i := 0; i <= len(rankings)-1; i++ {
+		ranking = rankings[i]
+		chance = ((float32(ranking.max) - float32(ranking.min)) / float32(maxRanking))
+		chance = chance * 100
+		fmt.Printf("\n\t%s for\t %2.2f %s", ranking.name, chance, "%")
+	}
+
 	// pick chance
 	seed := rand.NewSource(time.Now().UnixNano())
 	random := rand.New(seed)
 	pick := random.Intn(maxRanking)
-	fmt.Printf("pick is %v\n", pick)
 
 	// select food from pick
 	var pickedFood string
 	for i := 0; i <= len(rankings)-1; i++ {
-		rank := rankings[i]
-		if pick >= rank.min && pick <= rank.max {
-			pickedFood = rank.name
+		ranking := rankings[i]
+		if pick >= ranking.min && pick <= ranking.max {
+			pickedFood = ranking.name
 		}
 	}
-	// show picked food
-	fmt.Printf("You have to eat %s\n", pickedFood)
-	// update db
 
+	// show picked food
+	fmt.Printf("\nYou have to eat %s\n", pickedFood)
+
+	// update db
+	for i := 0; i <= len(foods)-1; i++ {
+		food := foods[i]
+		if food.name == pickedFood {
+			foods[i].rank++
+		}
+	}
+	// fmt.Println(foods)
 }
 
-// Food holds name and ranking
+// Food holds name and current ranking
 type Food struct {
 	name string
 	rank int
 }
 
-// Ranking holds food namen and min / max ranking
+// Ranking holds food name and rating range for pick
 type Ranking struct {
 	name string
 	min  int
 	max  int
-}
-
-// GetName return name of the food
-func (food Food) GetName() string {
-	return food.name
 }
